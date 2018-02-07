a33190dc31982e7388ce64cd4586c8728caf8cad
CesarValladares <cesar_valla@hotmail.com>
2018-02-07 13:08:15 -0600

    Temporal

--- Actividades/A5.1.Temporal/Temporal.sql
+++ Actividades/A5.1.Temporal/Temporal.sql
@@ -21,19 +21,45 @@ FOREIGN KEY (ID_Usuario) REFERENCES Usuarios(ID),
 fecha_inicio date not null, 
 fecha_fin date not null,
 cobertura smallint not null,
-costo int not null
+costo int not null,
+SYS_START TIMESTAMP(12) GENERATED ALWAYS AS ROW BEGIN NOT NULL,
+SYS_END TIMESTAMP(12) GENERATED ALWAYS AS ROW END NOT NULL,
+TRANS_START TIMESTAMP(12) GENERATED ALWAYS AS TRANSACTION START ID IMPLICITLY HIDDEN, 
+PERIOD SYSTEM_TIME(SYS_START,SYS_END)
 )
 
 create table Usuarios_hist like Usuarios
 
 alter table Usuarios add versioning use HISTORY table Usuarios_hist 
 
-Create TRIGGER Caneclacion 
-AFTER update on Polizas
+create table Polizas_hist like Polizas
+
+alter table Polizas add versioning use HISTORY table Polizas_hist
+
+Create TRIGGER Cancelacion
+AFTER update on POLIZAS
 referencing old as old_values new as new_values
 for each row mode db2sql
+when (new_values.fecha_fin < old_values.fecha_fin) 
+begin atomic 
+	declare meses int
+	declare meses2 int
+	declare diferencia int
+	declare costmes int
+	declare nmeses int
+	declare diferencia2 int
+	declare valorfin int
+	SET @meses = MONTH(old_values.fecha_inicio)
+	SET meses2 = MONTH(old_values.fecha_fin)
+	diferencia = meses2 - meses	
+	costmes = old_values.costo / diferencia
+	SET nmeses = MONTH(new_values.fecha_fin)
+	diferencia2 = nmeses - meses
+	valorfin = diferencia2 * costmes
+	new_value.cost = valorfin;end
 
 
 
+	
 
 
